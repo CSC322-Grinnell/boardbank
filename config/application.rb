bca14f76cb7a523e57e4c11513e54d4c703d5280
norabbuccino <bresette@grinnell.edu>
2014-10-10 12:30:34 -0300

    Updates for Heroku - see ESaas A.8

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module Boardbank
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # To prevent precompilation with Rails 3.X
+    config.assets.initialize_on_precompile = false
   end
 end
