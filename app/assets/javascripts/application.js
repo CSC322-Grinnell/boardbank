6c9112ee0cff9f3c2d1f2d80b481f6d58410c6d7
sivarama <sivarama@grinnell.edu>
2014-10-16 14:58:46 -0500

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+// app/assets/javascripts/application.js
+
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
