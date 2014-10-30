9a0502b3b345ad5d703b7fba98920b776d6d0139
Christine <tranchri@grinnell.edu>
2014-10-30 14:15:20 -0500

    Got rid of sprocket error when checking with rake assets:precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,11 @@ Boardbank::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+
+%w(stylesheets javascripts fonts images).each do |sub|
+  config.assets.paths << ::Bootstrap::Rails::Engine.root.join('assets', sub)
+end
+config.assets.precompile << %r(bootstrap/glyphicons-halflings-regular\.(?:eot|svg|ttf|woff)$)
+
 end
