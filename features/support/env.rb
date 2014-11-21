0f1c79307c527c193892266ebff87b8cabe7c399
Phineas <schlossb@grinnell.edu>
2014-11-20 20:45:56 -0600

    removed features from coverage

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,6 +9,7 @@ SimpleCov.start do
   add_filter '/config/'
   add_filter '/lib/'
   add_filter '/vendor/'
+  add_filter '/features/'
 
   add_group 'Controllers', 'app/controllers'
   add_group 'Models', 'app/models'
