f90e68544faeeaa8eb51ebbcf733df5f99a8fe8d
Christine Tran <tranchri@grinnell.edu>
2014-12-04 15:36:21 -0600

    Added fields to user sign up and edit page

---
+++ db/migrate/20141204195218_add_fields_to_user_signup.rb
@@ -0,0 +1,10 @@
+class AddFieldsToUserSignup < ActiveRecord::Migration
+  def change
+  	add_column :users, :address, :string
+  	add_column :users, :city, :string
+  	add_column :users, :state, :string
+  	add_column :users, :zipcode, :integer
+    add_column :users, :phonenumber, :integer
+    remove_column :users, :is_org, :string
+  end
+end
