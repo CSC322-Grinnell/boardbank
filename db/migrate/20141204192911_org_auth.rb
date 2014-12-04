8f867e4260568b09a0205b2e3a5724d975311821
Phineas <schlossb@grinnell.edu>
2014-12-04 15:35:33 -0600

    added confirmation to organizations/testing

---
+++ db/migrate/20141204192911_org_auth.rb
@@ -0,0 +1,11 @@
+class OrgAuth < ActiveRecord::Migration
+  def self.up
+    add_column :organizations, :approved, :boolean, :default => false, :null => false
+    add_index  :organizations, :approved
+  end
+
+  def self.down
+    remove_index  :organizations, :approved
+    remove_column :organizations, :approved
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141120214152) do
+ActiveRecord::Schema.define(:version => 20141204192911) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -47,22 +47,24 @@ ActiveRecord::Schema.define(:version => 20141120214152) do
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
   create_table "organizations", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0,  :null => false
+    t.integer  "sign_in_count",          :default => 0,     :null => false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
     t.string   "orgname"
     t.string   "orgabout"
+    t.boolean  "approved",               :default => false, :null => false
   end
 
+  add_index "organizations", ["approved"], :name => "index_organizations_on_approved"
   add_index "organizations", ["email"], :name => "index_organizations_on_email", :unique => true
   add_index "organizations", ["reset_password_token"], :name => "index_organizations_on_reset_password_token", :unique => true
 
