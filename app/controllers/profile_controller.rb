e784e90e24190add8a722e5296cb11b1a328a12e
saasbook <saasbook@saasbook.(none)>
2015-02-22 17:17:13 -0400

    added organization profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,11 +1,11 @@
 class ProfileController < ApplicationController
 
 def show
-  @profile = current_user || not_found
+  @profile = current_user || current_organization || not_found
 end
 
  def not_found
-    raise ActionController::RoutingError.new('User Not Found')
+    raise ActionController::RoutingError.new('Profile Not Found')
   end
 
 end
