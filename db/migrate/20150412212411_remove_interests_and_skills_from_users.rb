a6c62019ab27a1de9878a39c1c8dcea9854dd035
norabbuccino <bresette@grinnell.edu>
2015-05-11 18:03:29 -0300

    fixed skill migrations

--- db/migrate/20150412212411_remove_interests_and_skills_from_users.rb
+++ db/migrate/20150412212411_remove_interests_and_skills_from_users.rb
@@ -1,24 +1,6 @@
 class RemoveInterestsAndSkillsFromUsers < ActiveRecord::Migration
   def up
-    change_table :users do |t|
-      t.remove :interest_3
-      t.remove :interest_4
-      t.remove :interest_5
-      t.remove :advocacy
-      t.remove :business
-      t.remove :communications
-      t.remove :diversity
-      t.remove :finance
-      t.remove :fundraising
-      t.remove :humanresources
-      t.remove :information
-      t.remove :legal
-      t.remove :orgdevelop
-      t.remove :outcome
-      t.remove :projectmanagement
-      t.remove :training
-      t.remove :volunteer
-    end
+
   end
 
   def down
