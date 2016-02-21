93e17a65f712203aadb32ff9e7663de2dcc40414
Sooji Son <soojison96@gmail.com>
2016-02-20 21:22:44 -0600

    Fix warnings

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ end
 
 module Boardbank
   class Application < Rails::Application
+    config.active_record.raise_in_transactional_callbacks = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,7 @@
 Boardbank::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.eager_load = false
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
@@ -8,11 +9,11 @@ Boardbank::Application.configure do
   config.cache_classes = true
 
   # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_assets = true
+  config.serve_static_files = false
   config.static_cache_control = 'public, max-age=3600'
 
   # Log error messages when you accidentally call methods on nil
-  config.whiny_nils = false
+  #config.whiny_nils = false
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
