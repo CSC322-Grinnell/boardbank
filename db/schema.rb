f9794f595c47c3169f4cb07991555b039cc9dd3b
Noah Schlager <schlager@grinnell.edu>
2016-02-25 15:19:38 -0600

    Modules and css stuff

--- db/schema.rb
+++ db/schema.rb
@@ -9,63 +9,63 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150425215320) do
+ActiveRecord::Schema.define(version: 20150425215320) do
 
-  create_table "active_admin_comments", :force => true do |t|
-    t.string   "resource_id",   :null => false
-    t.string   "resource_type", :null => false
+  create_table "active_admin_comments", force: true do |t|
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
     t.integer  "author_id"
     t.string   "author_type"
     t.text     "body"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "namespace"
   end
 
-  add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
-  add_index "active_admin_comments", ["namespace"], :name => "index_active_admin_comments_on_namespace"
-  add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace"
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
 
-  create_table "admin_users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+  create_table "admin_users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0,  :null => false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
-  add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
+  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
 
-  create_table "interests", :force => true do |t|
+  create_table "interests", force: true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "organizations", :force => true do |t|
-    t.string   "email",                  :default => "",    :null => false
-    t.string   "encrypted_password",     :default => "",    :null => false
+  create_table "organizations", force: true do |t|
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0,     :null => false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
-    t.boolean  "approved",               :default => false, :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "approved",               default: false, null: false
     t.string   "orgname"
     t.string   "orgabout"
     t.string   "org_address"
@@ -76,46 +76,52 @@ ActiveRecord::Schema.define(:version => 20150425215320) do
     t.string   "org_contactname"
   end
 
-  add_index "organizations", ["approved"], :name => "index_organizations_on_approved"
-  add_index "organizations", ["email"], :name => "index_organizations_on_email", :unique => true
-  add_index "organizations", ["reset_password_token"], :name => "index_organizations_on_reset_password_token", :unique => true
+  add_index "organizations", ["approved"], name: "index_organizations_on_approved"
+  add_index "organizations", ["email"], name: "index_organizations_on_email", unique: true
+  add_index "organizations", ["reset_password_token"], name: "index_organizations_on_reset_password_token", unique: true
 
-  create_table "skills", :force => true do |t|
+  create_table "skills", force: true do |t|
     t.string   "name"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "skill_level"
   end
 
-  create_table "user_interests", :force => true do |t|
-    t.string   "user_id"
+  create_table "user_interests", force: true do |t|
+    t.integer  "user_id"
     t.integer  "interest_id"
-    t.boolean  "has_interest", :default => false
-    t.datetime "created_at",                      :null => false
-    t.datetime "updated_at",                      :null => false
+    t.boolean  "has_interest", default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "user_skills", :force => true do |t|
+  add_index "user_interests", ["interest_id"], name: "index_user_interests_on_interest_id"
+  add_index "user_interests", ["user_id"], name: "index_user_interests_on_user_id"
+
+  create_table "user_skills", force: true do |t|
     t.integer  "user_id"
     t.integer  "skill_id"
     t.string   "experience_level"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "",    :null => false
-    t.string   "encrypted_password",     :default => "",    :null => false
+  add_index "user_skills", ["skill_id"], name: "index_user_skills_on_skill_id"
+  add_index "user_skills", ["user_id"], name: "index_user_skills_on_user_id"
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0,     :null => false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "firstname"
     t.string   "lastname"
     t.string   "address"
@@ -125,14 +131,14 @@ ActiveRecord::Schema.define(:version => 20150425215320) do
     t.integer  "phonenumber"
     t.string   "education"
     t.string   "areaofstudy"
-    t.boolean  "previous_experience",    :default => false
-    t.boolean  "fundraise",              :default => false
-    t.boolean  "financial_contribution", :default => false
+    t.boolean  "previous_experience",    default: false
+    t.boolean  "fundraise",              default: false
+    t.boolean  "financial_contribution", default: false
     t.float    "availability"
     t.string   "additional_comments"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
