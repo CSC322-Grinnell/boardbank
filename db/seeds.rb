cb3425e60714fd6637ab2752f57c5c7676289a66
Mira <m13hall@gmail.com>
2016-04-20 16:31:03 -0500

    update skills. currently, weird display skills problem on edits page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(firstname: 'User', lastname: 'Man', email: 'user@exam.com', password: 'password', address: '123 Main Street', city: 'Grinnell', state: 'Iowa', zipcode: '50112', phonenumber: '1234567891', education: 'Bachelors')
+user = User.create(firstname: 'User', lastname: 'Man', email: 'user@exam.com', 
+	password: 'password', address: '123 Main Street', city: 'Grinnell', 
+	state: 'Iowa', zipcode: '50112', phonenumber: '1234567891', 
+	education: 'Bachelors')
 User.create(firstname: 'Aaa', lastname: 'Bbb', email: 'aaabbb@example.com', password: 'password')
 User.create(firstname: 'Bob', lastname: 'Smith', email: 'bobsmith@example.com', password: 'password')
 User.create(firstname: 'Jane', lastname: 'Doe', email: 'janedoe@example.com', password: 'password')
@@ -17,5 +20,8 @@ Organization.create(orgname: 'Animal Shelter', orgabout: 'Rescuing stray animals
 Organization.create(orgname: 'Lovely', orgabout: 'Nerb dating app', email: 'l0ve@example.com', password: 'password')
 
 Skill.create([{ name: 'Advocacy or Governmental Relations' }, { name: 'Business or Management' }, { name: 'Communications' }, { name: 'Diversity Issues' }, { name: 'Finance and Accounting' }, { name: 'Fundraising' }, { name: 'Human Resources' }, { name: 'Information Technology' }, { name: 'Legal' }, { name: 'Organization Development' }, { name: 'Outcome Measurement or Statistical Analysis' }, { name: 'Project Management' }, { name: 'Training and Development' }, { name: 'Volunteer Management' }])
+skill = Skill.find_by_id(1)
+user.user_skills.create(skill_id: '1', experience_level: 'Some Experience')
+
 
 Interest.create([{ name: 'Arts/Museum' }, { name: 'Early Childhood' }, { name: 'Literacy' }, { name: 'Animal Rights' }, { name: 'Environmental' }, { name: 'Mental Health' }, { name: 'Children/Youth' }, { name: 'Health Care' }, { name: 'Recreation' }, { name: 'Civic/Community' }, { name: 'Historical' }, { name: 'Preservation' }, { name: 'Senior Services' }, { name: 'Disabilities' }, { name: 'Homeless/Emergency' }, { name: 'Substance Abuse' }, { name: 'Education' }, { name: 'Housing Development' }])
