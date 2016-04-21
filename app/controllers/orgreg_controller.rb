61c5e352eea6d982e306f5dde399453116d081df
Sooji Son <soojison96@gmail.com>
2016-04-21 13:27:28 -0500

    orgs can update their passwords, or not

--- app/controllers/orgreg_controller.rb
+++ app/controllers/orgreg_controller.rb
@@ -16,7 +16,13 @@ class OrgregController < Devise::RegistrationsController
 
   def update
     byebug
-    org_params = params.require(:organization).permit(:orgname, :orgabout, :org_address, :org_city, :org_state, :org_zipcode, :org_telephone, :org_contactname, :email)
+    org_params = params.require(:organization).permit(:password, :password_confirmation, :orgname, :orgabout, :org_address, :org_city, :org_state, :org_zipcode, :org_telephone, :org_contactname, :email)
+    #password cannot be blank, so what if the org doesn't want to update the password?
+    if org_params[:password].empty? and org_params[:password_confirmation].empty?
+      org_params.extract!(:password, :password_confirmation)
+    end
+
+    #otherwise update the attributes
     if @organization.update_attributes(org_params)
       #handle successful update
       redirect_to :root
