949a6e759f1e3f5b9ec4a549e604c8cf60161e87
Mira <m13hall@gmail.com>
2016-04-21 14:31:57 -0500

    only show skills where user has more experience than 'none', update seeds to match new experience level strings

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ Organization.create(orgname: 'Lovely', orgabout: 'Nerb dating app', email: 'l0ve
 
 Skill.create([{ name: 'Advocacy or Governmental Relations' }, { name: 'Business or Management' }, { name: 'Communications' }, { name: 'Diversity Issues' }, { name: 'Finance and Accounting' }, { name: 'Fundraising' }, { name: 'Human Resources' }, { name: 'Information Technology' }, { name: 'Legal' }, { name: 'Organization Development' }, { name: 'Outcome Measurement or Statistical Analysis' }, { name: 'Project Management' }, { name: 'Training and Development' }, { name: 'Volunteer Management' }])
 skill = Skill.find_by_id(1)
-user.user_skills.create(skill_id: '1', experience_level: 'Some Experience')
+user.user_skills.create(skill_id: '1', experience_level: 'Some')
 
 
 Interest.create([{ name: 'Arts/Museum' }, { name: 'Early Childhood' }, { name: 'Literacy' }, { name: 'Animal Rights' }, { name: 'Environmental' }, { name: 'Mental Health' }, { name: 'Children/Youth' }, { name: 'Health Care' }, { name: 'Recreation' }, { name: 'Civic/Community' }, { name: 'Historical' }, { name: 'Preservation' }, { name: 'Senior Services' }, { name: 'Disabilities' }, { name: 'Homeless/Emergency' }, { name: 'Substance Abuse' }, { name: 'Education' }, { name: 'Housing Development' }])
