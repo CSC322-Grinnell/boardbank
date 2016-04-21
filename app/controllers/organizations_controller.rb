9f703825c8c85a111e026cfd50c3b6e072e8189d
Sooji Son <soojison96@gmail.com>
2016-04-21 13:05:25 -0500

    start fixing controllers to follow rails conventions

---
+++ app/controllers/organizations_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+
+  def edit
+    #@interests = Interest.all
+    #@skills = Skill.all
+    #@user_skills = Hash[resource.user_skills.map { |user_skill| [user_skill.skill_id, user_skill] }]
+  end
+
+  def update
+    org_params = params.require(:organization).permit(:orgname, :orgabout, :org_address, :org_city, :org_state, :org_zipcode, :org_telephone, :org_contactname)
+    if @organization.update_attributes(org_params)
+      #handle successful update
+      redirect_to :root
+    else
+      redirect_to @organization
+    end
+  end
+
+  def show
+    @org = Organization.find(params[:id])
+    unless @org == current_organization
+      redirect_to '/'
+    end
+  end
+end
