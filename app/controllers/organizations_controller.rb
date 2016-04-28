dfaa972c12c6e42e82d3de8644b1c86863c53aa3
Sooji Son <soojison96@gmail.com>
2016-04-28 13:35:20 -0500

    add comments

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,32 +1,35 @@
 class OrganizationsController < Devise::RegistrationsController
-  #this controller currently does nothing
+
   def index
     @organizations = Organization.all
   end
 
+  #shows organization's profile page
   def show
     @org = Organization.find(params[:id])
-    #crap, I just realized you should be able to view other org's profiles
-    #unless @org == current_organization
-    #  redirect_to '/'
-    #end
   end
 
+  #creates a new organization - sign up page
   def new
     super
   end
 
+  #edit profile page
   def edit
     super
   end
 
+  #creates a new organization - saves to the database
   def create
+    byebug
     super
   end
 
+  #updates the edits from the edit profile page
   def update
     org_params = params.require(:organization).permit(:password, :password_confirmation, :orgname, :orgabout, :org_address, :org_city, :org_state, :org_zipcode, :org_telephone, :org_contactname, :email)
     #password cannot be blank, so what if the org doesn't want to update the password?
+    #we make sure nothing gets updated in the password field then.
     if org_params[:password].empty? and org_params[:password_confirmation].empty?
       org_params.extract!(:password, :password_confirmation)
     end
@@ -45,7 +48,9 @@ class OrganizationsController < Devise::RegistrationsController
     end
   end
 
+  #deletes organization's profile
   def destroy
+    byebug
     super
   end
 
