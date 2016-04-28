ab2f5f69842716cb64135bbd8036cedaaa38aafd
Sooji Son <soojison96@gmail.com>
2016-04-27 19:29:42 -0500

    well, at least I fixed one problem

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,20 +1,6 @@
 class OrganizationsController < ApplicationController
-
-  def edit
-    #@interests = Interest.all
-    #@skills = Skill.all
-    #@user_skills = Hash[resource.user_skills.map { |user_skill| [user_skill.skill_id, user_skill] }]
-  end
-
-  def update
-    org_params = params.require(:organization).permit(:orgname, :orgabout, :org_address, :org_city, :org_state, :org_zipcode, :org_telephone, :org_contactname)
-    if @organization.update_attributes(org_params)
-      #handle successful update
-      byebug
-      redirect_to @organization
-    else
-      redirect_to @organization
-    end
+  #this controller currently does nothing
+  def index
   end
 
   def show
@@ -23,4 +9,22 @@ class OrganizationsController < ApplicationController
       redirect_to '/'
     end
   end
+
+  def new
+  end
+
+  def edit
+    super
+  end
+
+  def create
+  end
+
+  def update
+    @organization = Organization.find(params[:id])
+  end
+
+  def destroy
+  end
+
 end
--- app/controllers/orglist_controller.rb
+++ app/controllers/orglist_controller.rb
@@ -1,17 +1,17 @@
 class OrglistController < ApplicationController
 
   #@orgs = Organization.all
-  
+
   def show
-    
-    @orgs = Organization.search params[:q] if params[:q].present?  
+
+    @orgs = Organization.search params[:q] if params[:q].present?
     @orgs = Organization.all if !(params[:q]).present?
     #@orgs ||= Organization.search "Happy"
     @categories = ['']
     @all_categories = ['Arts/Musuem', 'Early Childhood', 'Literacy', 'Animal Rights', 'Environmental', 'Mental Health', 'Children/Youth', 'Health Care', 'Recreation', 'Civic/Community', 'Historical', 'Preservation', 'Senior Services', 'Disabilities', 'Homeless/Emergency', 'Substance Abuse', 'Education', 'Housing Development']
  end
- 
- 
+
+
 # def search_path
 #   q = params[:q]
 #   @orgs = Organization.search "Happy"
