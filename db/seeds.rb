2f1dd568fe175e576805ba13f7523c4cb81fabec
Sooji Son <soojison96@gmail.com>
2016-05-05 14:33:21 -0500

    change org column names to a much simpler name

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.create(firstname: 'User', lastname: 'Man', email: 'user@exam.com', 
-	password: 'password', address: '123 Main Street', city: 'Grinnell', 
-	state: 'Iowa', zipcode: '50112', phonenumber: '1234567891', 
+user = User.create(firstname: 'User', lastname: 'Man', email: 'user@exam.com',
+	password: 'password', address: '123 Main Street', city: 'Grinnell',
+	state: 'Iowa', zipcode: '50112', phonenumber: '1234567891',
 	education: 'Bachelors')
 User.create(firstname: 'Aaa', lastname: 'Bbb', email: 'aaabbb@example.com', password: 'password')
 User.create(firstname: 'Bob', lastname: 'Smith', email: 'bobsmith@example.com', password: 'password')
 User.create(firstname: 'Jane', lastname: 'Doe', email: 'janedoe@example.com', password: 'password')
 
-Organization.create(orgname: 'Org', orgabout: 'An organization for organizations', email: 'org@example.com', password: 'password')
-Organization.create(orgname: 'Happy Makers', orgabout: 'We make people happy', email: 'happy@example.com', password: 'password')
-Organization.create(orgname: 'Animal Shelter', orgabout: 'Rescuing stray animals', email: 'animals@example.com', password: 'password')
-Organization.create(orgname: 'Lovely', orgabout: 'Nerb dating app', email: 'l0ve@example.com', password: 'password')
+Organization.create!(name: 'Org', about: 'An organization for organizations', email: 'org@example.com', password: 'password')
+Organization.create!(name: 'Happy Makers', about: 'We make people happy', email: 'happy@example.com', password: 'password')
+Organization.create!(name: 'Animal Shelter', about: 'Rescuing stray animals', email: 'animals@example.com', password: 'password')
+Organization.create!(name: 'Lovely', about: 'Nerb dating app', email: 'l0ve@example.com', password: 'password')
 
 Skill.create([{ name: 'Advocacy or Governmental Relations' }, { name: 'Business or Management' }, { name: 'Communications' }, { name: 'Diversity Issues' }, { name: 'Finance and Accounting' }, { name: 'Fundraising' }, { name: 'Human Resources' }, { name: 'Information Technology' }, { name: 'Legal' }, { name: 'Organization Development' }, { name: 'Outcome Measurement or Statistical Analysis' }, { name: 'Project Management' }, { name: 'Training and Development' }, { name: 'Volunteer Management' }])
 user.user_skills.create(skill_id: '1', experience_level: 'Some')
