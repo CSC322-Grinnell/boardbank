375c975e3d7e79d0dcbecfd4ca977e5c6585b245
Sooji Son <soojison96@gmail.com>
2016-05-06 14:57:31 -0500

    edit more files to match model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,21 +7,5 @@ class ApplicationController < ActionController::Base
     else
       redirect_to '/' # placeholder before i can figure out the org thing
     end
-    #render :json => {"ErrorType" => "Record Not Found", "message" => e.message}
   end
-  #  protected
-
-  #  def configure_devise_permitted_parameters
-  #    registration_params = [:org_contactname, :org_address, :org_telephone, :org_city, :org_state, :org_zipcode, :orgname, :orgabout, :firstname, :lastname, :is_org, :email, :password, :password_confirmation]
-
-  #    if params[:action] == 'update'
-  #      devise_parameter_sanitizer.for(:account_update) {
-  #        |u| u.permit(registration_params << :current_password)
-  #    }
-  #    elsif params[:action] == 'create'
-  #      devise_parameter_sanitizer.for(:sign_up) {
-  #        |u| u.permit(registration_params)
-  #      }
-  #    end
-  #  end
 end
