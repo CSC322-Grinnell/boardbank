2deb164080a3d8da2eba4fe8a5c47532c260a1a4
Zachary J. Susag <susagzac@grinnell.edu>
2018-11-12 10:47:13 -0600

    Undo renaming of AdminUser

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,4 +1,4 @@
-class Adminvolunteer < ActiveRecord::Base
+class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
