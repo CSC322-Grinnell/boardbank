2f808e84af5054f778ada7a49277b8c38adea443
Zachary J. Susag <susagzac@grinnell.edu>
2018-11-12 14:07:38 -0600

    Migrate from deprecated FactoryGirl gem to FactoryBot gem

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,4 +1,4 @@
 # For RSpec, create this file as spec/support/factory_girl.rb
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
 end
