a690163bd8aa80c18aff3aa14bb2330bffa4f4eb
Zachary J. Susag <susagzac@grinnell.edu>
2018-11-19 21:13:16 -0600

    Use strong parameters for the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,23 +61,25 @@ class UsersController < Devise::RegistrationsController
   # PUT /users/1
   # PUT /users/1.json
   def update
-    if user_params[:state].empty?
-      user_params.extract!(:state)
+    updated_info = user_params
+    
+    if updated_info[:state].empty?
+      updated_info.extract!(:state)
     end
-    if user_params[:education].empty?
-      user_params.extract!(:education)
+    if updated_info[:education].empty?
+      updated_info.extract!(:education)
     end
     
     require_password = true
-    if user_params[:current_password].empty? and user_params[:password_confirmation].empty? and user_params[:password].empty?
-      user_params.extract!(:current_password, :password_confirmation, :password)
+    if updated_info[:current_password].empty? and updated_info[:password_confirmation].empty? and updated_info[:password].empty?
+      updated_info.extract!(:current_password, :password_confirmation, :password)
       require_password = false
     end
     
     
     #debugger
     
-    if (require_password and @user.update_with_password(user_params)) or ((not require_password) and @user.update_without_password(user_params))
+    if ((not require_password) and @user.update_without_password(updated_info)) or (require_password and @user.update_with_password(updated_info))
       sign_in(@user, :bypass => true)
       #only update skills if password not required or supplied correctly 
       
@@ -174,6 +176,6 @@ class UsersController < Devise::RegistrationsController
                   :email, :password, :password_confirmation, :remember_me,
                   :fundraise, :financial_contribution, :availability,
                   :additional_comments, :user_skills, :user_skills_attributes,
-                  :shownumber)
+                  :current_password, :shownumber)
   end
 end
