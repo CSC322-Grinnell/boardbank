eda3f61453cd2c4d02835ad7c97bee0da8dd5d70
Zachary J. Susag <susagzac@grinnell.edu>
2018-11-21 10:48:50 -0600

    Update application to use boolean symbols instead of integers

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ end
 
 module Boardbank
   class Application < Rails::Application
+    Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
