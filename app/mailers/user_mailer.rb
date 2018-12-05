076150654da48adb3a03345ccde7dcdf0b09d4b5
Zachary J. Susag <susagzac@grinnell.edu>
2018-12-05 11:35:52 -0600

    Configure mailer to send password reset emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,8 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.password_reset.subject
   #
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password Reset"
   end
 end
