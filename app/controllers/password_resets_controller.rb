b252aba6ef00c43c9364e09e1b8ddd235bf5365d
Zachary J. Susag <susagzac@grinnell.edu>
2018-12-05 11:14:53 -0600

    Add method to search database for user with given email for password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,30 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    @org  = Organizations.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      process_request(@user)
+    elsif  @org
+      process_request(@org)
+    else
+      flash.now[:danger] = "Email address not found"
+      render 'new'
+    end
+  end
+  
   def edit
   end
+
+  private
+
+  def process_request(user)
+    user.create_reset_digest
+    user.send_password_reset_email
+    flash[:info] = "Email sent with password reset instructions"
+    redirect_to root_url
+  end
+  
+  
 end
