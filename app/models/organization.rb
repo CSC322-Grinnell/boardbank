880b76a8ca3410c46107fe4666e680173f0a9f68
Zachary J. Susag <susagzac@grinnell.edu>
2018-12-05 11:14:32 -0600

    Update models to create password reset tokens

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,8 +1,8 @@
 class Organization < ApplicationRecord
+  attr_accessor :reset_token
 
   include Elasticsearch::Model
   searchkick
-
 
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable, :registerable
@@ -17,6 +17,18 @@ class Organization < ApplicationRecord
     else
       super # Use whatever other message
     end
+  end
+
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = Organizations.new_token
+    update_attribute(:reset_digest,  Organizations.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  
+  attr_accessor :reset_token
+
   include Elasticsearch::Model
   searchkick
   
@@ -20,5 +21,16 @@ class User < ApplicationRecord
   def phonenumber=(phonenumber)
     self[:phonenumber] = phonenumber.gsub(/\D/, '')
   end #http://stackoverflow.com/questions/10214950/how-to-format-values-before-saving-to-database-in-rails-3
-  
+
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
 end
